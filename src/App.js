00c32dd41501c0b24f418ea96f1823956701eb53
abbas495 <aalsory64@hotmail.com>
2020-12-04 10:57:40 +0100

    creating Todo App using React and Material-Ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { v4 } from "uuid";
+import TodoForm from "./components/TodoForms";
+import TodoList from "./components/TodoList";
+import { useState } from "react";
+import { FormatBoldRounded } from "@material-ui/icons";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const checkTodo = (id) => {
+    console.log(id);
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) todo.isCompleted = !todo.isCompleted;
+        console.log(todo.isCompleted);
+        return todo;
+      })
+    );
+  };
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id == !id));
+  };
+  //Add a todo
+  const addTodo = (text) => {
+    const newTodo = {
+      id: v4(),
+      title: text,
+      isCompleted: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} checkTodo={checkTodo} deleteTodo={deleteTodo} />
     </div>
   );
 }
